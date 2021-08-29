7446d4582dd21971c7268d3c124b247482da6048
DIY Everything VN <63512636+diyeverythingvn@users.noreply.github.com>
2021-08-29 12:59:40 +0700

    Add files via upload

---
+++ MMM-AmLich.js
@@ -0,0 +1,30 @@
+Module.register("MMM-AmLich",{
+	defaults: {
+        size: 'small'
+	},
+    
+	getScripts: function() {
+		return [
+			this.file('AmLich.js')
+		]
+	},
+	
+	getStyles: function() {
+		return [
+			this.file('MMM-AmLich.css')
+		]
+	},
+
+	getDom: function() {
+		var wrapper = document.createElement("div");
+        if (this.config.size == 'big' || this.config.size == '') {
+            wrapper.innerHTML = printToday();
+        } else if (this.config.size == 'medium') {
+            wrapper.innerHTML = printTodayMedium();
+        } else if (this.config.size == 'small') {
+            wrapper.innerHTML = printTodaySmall();
+        }
+		return wrapper;
+	}
+	
+});
